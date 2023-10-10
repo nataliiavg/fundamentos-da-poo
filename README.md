34bfff46890a67d3e4341932fb6bcbb409ca3fcd
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-09 21:07:11 -0300

    Add content about class

---
+++ README.md
@@ -0,0 +1,13 @@
+## Bootcamp Santander FullStack em parceria com a DIO
+
+### Fundamentos da Programação Orientada a Objetos com Java:
+
+- Classes
+- Pacotes
+- Visibilidade dos recursos
+- Getters e Setters
+- Construtores
+- Enums
+- UML
+
+##
---
+++ src/classes/Student.java
@@ -0,0 +1,25 @@
+package classes;
+
+// Criando a classe Student
+// Com todas as características e comportamentos aplicados
+
+public class Student {
+
+    String name;
+    int age;
+    Color color;
+    Sex sex;
+
+    void eating(Food food) {
+        // Código Aqui
+    }
+
+    void drinking(Drink drink) {
+        // Código Aqui
+
+    }
+
+    void running() {
+
+    }
+}
