7c267bae39c4810150a37a0032b5bdccd513a8ee
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-11 10:05:24 -0300

    Add standard JavaBeans

--- src/getters_e_setters/escola/Escola.java
+++ src/getters_e_setters/escola/Escola.java
@@ -4,10 +4,10 @@ public class Escola {
     public static void main(String[] args) {
         Aluno felipe = new Aluno();
 
-        felipe.nome = "Felipe";
-        felipe.idade = 8;
+        felipe.setNome("Jorge Lucas");
+        felipe.setIdade(12);
 
-        System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos.");
+        System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos.");
 
     }
 }
