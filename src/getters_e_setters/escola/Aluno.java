1309cb732c99a665b743da8c3039c1e4be4add91
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-11 09:52:40 -0300

    Add get and set examples

---
+++ src/getters_e_setters/escola/Aluno.java
@@ -0,0 +1,6 @@
+package getters_e_setters.escola;
+
+public class Aluno{
+    String nome; 
+    int idade;
+}
---
+++ src/getters_e_setters/escola/Escola.java
@@ -0,0 +1,13 @@
+package getters_e_setters.escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+
+        felipe.nome = "Felipe";
+        felipe.idade = 8;
+
+        System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos.");
+
+    }
+}
